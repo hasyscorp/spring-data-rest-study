290ff90624535f96a9627a3b9a97ed2f0b82a2b3
ian-jo <ian.jo@kakaocorp.com>
2021-01-22 16:49:46 +0900

    chore: initial project

---
+++ src/main/java/kr/co/hasy/springdatareststudy/SpringDataRestStudyApplication.java
@@ -0,0 +1,13 @@
+package kr.co.hasy.springdatareststudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataRestStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataRestStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/hasy/springdatareststudy/SpringDataRestStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.hasy.springdatareststudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataRestStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
