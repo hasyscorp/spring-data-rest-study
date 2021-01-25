c08280b2864d419b45a4a5b9816c7281b83dbfd4
ian-jo <ian.jo@kakaocorp.com>
2021-01-25 14:39:04 +0900

    projection 추가

---
+++ src/main/java/kr/co/hasy/springdatareststudy/shop/ShopResponse.java
@@ -0,0 +1,11 @@
+package kr.co.hasy.springdatareststudy.shop;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(name = "shopResponse", types = Shop.class)
+public interface ShopResponse {
+
+    @Value("#{target.name} #{target.branchName}")
+    String getFullName();
+}
