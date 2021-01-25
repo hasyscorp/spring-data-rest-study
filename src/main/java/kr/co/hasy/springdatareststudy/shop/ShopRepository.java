944635424af3d1230bbab67cf64d57592bc92839
ian-jo <ian.jo@kakaocorp.com>
2021-01-25 14:18:10 +0900

    feat: 검색 API 추가

--- src/main/java/kr/co/hasy/springdatareststudy/shop/ShopRepository.java
+++ src/main/java/kr/co/hasy/springdatareststudy/shop/ShopRepository.java
@@ -1,6 +1,14 @@
 package kr.co.hasy.springdatareststudy.shop;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 public interface ShopRepository extends JpaRepository<Shop, Long> {
+    @RestResource(path = "name")
+    List<Shop> findAllByName(String name);
+    @RestResource(path = "branchName")
+    List<Shop> findAllByBranchName(String branchName);
+
 }
