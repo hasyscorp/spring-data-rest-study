d8eeb66ca5a95ed3371e921ededde6b7954fa293
ian-jo <ian.jo@kakaocorp.com>
2021-01-25 14:29:20 +0900

    feat: 페이징, 정렬 기능 추가

--- src/main/java/kr/co/hasy/springdatareststudy/shop/ShopRepository.java
+++ src/main/java/kr/co/hasy/springdatareststudy/shop/ShopRepository.java
@@ -1,11 +1,11 @@
 package kr.co.hasy.springdatareststudy.shop;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 
-public interface ShopRepository extends JpaRepository<Shop, Long> {
+public interface ShopRepository extends PagingAndSortingRepository<Shop, Long> {
     @RestResource(path = "name")
     List<Shop> findAllByName(String name);
     @RestResource(path = "branchName")
